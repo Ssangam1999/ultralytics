2397b47ace6e3f478e08a3514fad1301db4a61c7
Ssangam1999 <sangam.poudel2016@gmail.com>
2024-06-06 17:35:08 +0545

    Changes for auto annotation

---
+++ examples/auto_annotation.py
@@ -0,0 +1,19 @@
+from ultralytics.models.yolo.detect.predict import DetectionPredictor
+
+
+import os
+
+
+# Path to folder containing images
+image_folder = '/home/vertex/Documents/modified_leaf_dataset/Leaf detection.v3i.yolov8/test/images'
+
+# Path to output folder
+output_folder = '/path/to/output/folder'
+
+# Iterate over each image in the folder
+for image_file in os.listdir(image_folder):
+        image_path = os.path.join(image_folder, image_file)
+        args = dict(model='/home/vertex/Downloads/plant_only.pt', source=image_path)
+        predictor = DetectionPredictor(overrides=args)
+        predictor.predict_cli()
+
